6253db714f5b4e5f36f67c5903d404466dadc8c5
Nick Mascia <nmmascia@gmail.com>
2019-09-18 11:39:38 -0400

    Export plain backend

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import MouseBackend from './MouseBackend';
 import * as NativeTypes from './react-dnd-html5-backend/NativeTypes';
+
+import MouseBackend from './MouseBackend';
 import getEmptyImage from './react-dnd-html5-backend/getEmptyImage';
 
 const createMouseBackend = manager => new MouseBackend(manager);
 
 export default createMouseBackend;
-
-export {NativeTypes, getEmptyImage};
+export {NativeTypes, getEmptyImage, MouseBackend};
